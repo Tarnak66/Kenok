fa37be49b8256ad1f9c178984d38d0a20559a1b3
Tarnak66 <bestjorko@gmail.com>
2026-09-12 17:01:36 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -212,7 +212,7 @@ else:
                 with st.spinner("Kenok мисли..."):
                     try:
                         res = client.chat.completions.create(
-                            model="llama-3.1-70b-versatile",
+                            model="llama-3.3-70b-versatile",
                             messages=[{"role": "system", "content": SYSTEM_INSTRUCTIONS}] + curr["messages"][-10:]
                         ).choices[0].message.content
                         st.write(res)
